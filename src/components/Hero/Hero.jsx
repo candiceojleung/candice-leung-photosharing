323112be9910df757aa9ddb4b1b5cc789aa5a46a
candiceojleung <candice.leung@live.ca>
2025-02-14 17:23:43 -0500

    "add hero div and styling"

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,11 +3,13 @@ import "./Hero.scss";
 function Hero() {
   return (
     <>
+    <div className="hero">
       <p className="hero__title">Our Mission:</p>
       <p className="hero__paragraph">
         Provide photographers a space to share photos of the neighborhoods they
         cherish, <span className= "hero__paragraph--italics">expressed in their unique style.</span> 
       </p>
+      </div>
     </>
   );
 }
