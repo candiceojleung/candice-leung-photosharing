612de87928de28799d3b19540cc4e530e365f4c7
candiceojleung <candice.leung@live.ca>
2025-02-14 12:36:54 -0500

    create basic styling for filter buttons and alignment of header components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,13 @@
 import FilterButton from "../FilterButton/FilterButton";
+import FilterDrawer from "../FilterDrawer/FilterDrawer";
 import "./Header.scss";
 
-function Header({toggleDrawer}) {
+function Header({ toggleDrawer }) {
   return (
     <div className="header">
       <h1 className="header__title">Snaps</h1>
       <div className="header__button">
-        <FilterButton onClick={toggleDrawer}/>
+        <FilterButton onClick={toggleDrawer} />
       </div>
     </div>
   );
