c5c3e1f098f07994bdf02c631065565d68fc5883
candiceojleung <candice.leung@live.ca>
2025-02-14 22:33:30 -0500

    add additional function so that when drawer is open, filter button takes on different styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,12 @@
 import FilterButton from "../FilterButton/FilterButton";
 import "./Header.scss";
 
-function Header({ toggleDrawer }) {
+function Header({ toggleDrawer, isDrawerOpen }) {
   return (
     <div className="header">
       <h1 className="header__title">Snaps</h1>
       <div className="header__button">
-        <FilterButton onClick={toggleDrawer} />
+        <FilterButton onClick={toggleDrawer} isDrawerOpen={isDrawerOpen} />
       </div>
     </div>
   );
