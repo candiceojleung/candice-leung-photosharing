cdb0dd0f05662c1d523d040402e86d5dfe6e1739
candiceojleung <candice.leung@live.ca>
2025-02-20 23:27:27 -0500

    add font style to comment section

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -1,13 +1,14 @@
 import "./Comment.scss";
+import { SmallBodyCopy, BodyCopy} from "../../components/Typography/Typography"
 
 function Comment({name,time,comment}) {
   return (
     <section className="comment__container">
       <div className="comment__item">
-        <p className="comment__name">{name}</p>
-        <p className="comment__date">{time}</p>
+        <p className="comment__name"><SmallBodyCopy>{name}</SmallBodyCopy></p>
+        <p className="comment__date"><SmallBodyCopy>{time}</SmallBodyCopy></p>
       </div>
-      <p className="comment__text">{comment}</p>
+      <p className="comment__text"><BodyCopy>{comment}</BodyCopy></p>
     </section>
   );
 }
