3db707d17468853afa87bcc31870cf12baea9081
candiceojleung <candice.leung@live.ca>
2025-02-20 23:28:27 -0500

    add preliminary mobile styling and add in date settings for locale date string to match photocard

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,6 +1,6 @@
 import "./CommentList.scss";
 import Comment from "../Comment/Comment";
-
+import { BodyCopy } from "../Typography/Typography";
 
 function CommentList({ comments }) {
   if (!comments) {
@@ -8,16 +8,34 @@ function CommentList({ comments }) {
   }
   console.log(comments);
 
+  const commentCount = comments.length;
+  const commentText = commentCount === 1 ? "Comment" : "Comments";
+
+  const options = {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+  };
+
   return (
     <section className="comment-list">
+      <div className="comment-list__length">
+        <BodyCopy>{commentCount} {commentText} </BodyCopy>
+      </div>
       {comments.map((comment) => {
-        return(
-        <Comment
-          key={comment.id}
-          name={comment.name}
-          time={new Date(comment.timestamp).toLocaleDateString()}
-          comment={comment.comment}
-        />);
+        const formattedTime = new Date(comment.timestamp).toLocaleDateString(
+          "en-US",
+          options
+        );
+
+        return (
+          <Comment
+            key={comment.id}
+            name={comment.name}
+            time={formattedTime}
+            comment={comment.comment}
+          />
+        );
       })}
     </section>
   );
