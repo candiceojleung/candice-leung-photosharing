93244cb43afed2ac896d2a28b68ce746ff7756cf
candiceojleung <candice.leung@live.ca>
2025-02-25 22:10:26 -0500

    import photos from server

--- src/components/PhotoCardList/PhotoCardList.jsx
+++ src/components/PhotoCardList/PhotoCardList.jsx
@@ -5,9 +5,6 @@ import PhotoCard from "../PhotoCard/PhotoCard";
 
 function PhotoCardList({ selectedFilter, isDrawerOpen }) {
   const [photos, setPhotos] = useState(null);
-  // const BASE_URL = "https://unit-3-project-c5faaab51857.herokuapp.com";
-  // const API_KEY = "0b7ea1c0-7c37-4087-bfb3-dd00663da892";
-  // const url = `${BASE_URL}/photos?api_key=${API_KEY}`;
 
   useEffect(() => {
     fetchPhotos();
