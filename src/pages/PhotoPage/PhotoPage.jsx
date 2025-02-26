87b40110d4fe9667dd1a91e5cd749ce78b87902e
candiceojleung <candice.leung@live.ca>
2025-02-25 22:11:26 -0500

    get comments from server

--- src/pages/PhotoPage/PhotoPage.jsx
+++ src/pages/PhotoPage/PhotoPage.jsx
@@ -11,9 +11,6 @@ import PhotoPageHeader from "../../components/PhotoPageHeader/PhotoPageHeader";
 export default function PhotoPage() {
   const { id } = useParams();
   const [comments, setComments] = useState(null);
-  const BASE_URL = "https://unit-3-project-c5faaab51857.herokuapp.com";
-  const API_KEY = "0b7ea1c0-7c37-4087-bfb3-dd00663da892";
-  const URL = `${BASE_URL}/photos/${id}/comments?api_key=${API_KEY}`; //fetch comments in comment component, and in form component
 
   useEffect(() => {
     fetchComments();
@@ -21,7 +18,7 @@ export default function PhotoPage() {
 
   async function fetchComments() {
     try {
-      const { data } = await axios.get(URL);
+      const { data } = await axios.get(`${import.meta.env.VITE_BASE_URL}/photos/${id}/comments`);
       const sortedData = data.sort(
         (a, b) => new Date(b.timestamp) - new Date(a.timestamp) //sort comments based on date posted, showing most recent at top
       );
