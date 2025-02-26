c836f1bdc7a32ee994de56393d8c0f111f8ef25b
candiceojleung <candice.leung@live.ca>
2025-02-25 22:12:15 -0500

    import post function from server

--- src/components/PhotoPageForm/PhotoPageForm.jsx
+++ src/components/PhotoPageForm/PhotoPageForm.jsx
@@ -4,9 +4,6 @@ import axios from "axios";
 import { BodyCopy } from "../Typography/Typography";
 
 function PhotoPageForm({ id, fetchComments }) {
-  const BASE_URL = "https://unit-3-project-c5faaab51857.herokuapp.com";
-  const API_KEY = "0b7ea1c0-7c37-4087-bfb3-dd00663da892";
-  const URL = `${BASE_URL}/photos/${id}/comments?api_key=${API_KEY}`; //set up api key url
   const [name, setName] = useState("");
   const [comment, setComment] = useState("");
   const [submit, setSubmit] = useState(false);
@@ -34,7 +31,7 @@ function PhotoPageForm({ id, fetchComments }) {
   async function postComment() {
     try {
       const commentObj = { name: name, comment: comment };
-      const response = await axios.post(URL, commentObj);
+      const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/photos/${id}/comments`, commentObj);
       fetchComments(); //fetch comments again with new comment
       setName(""); //reset name field
       setComment(""); //reset comment field
